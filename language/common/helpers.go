535be6ea44fd13c2d2b29eb4a90fb3804793f828
Ryan Liu <azhai@126.com>
2020-04-25 12:11:33 +0800

    增加几个辅助函数

--- language/common/helpers.go
+++ language/common/helpers.go
@@ -9,35 +9,34 @@ import (
  */
 type FilterFunc = func(query *xorm.Session) *xorm.Session
 
-/**
- * 翻页查询，out参数需要传引用
- * 使用方法 total, err := Paginate(query, &rows, pageno, pagesize)
- */
-func Paginate(query *xorm.Session, out interface{}, pageno, pagesize int) (int, error) {
-	var (
-		total int64
-		err   error
-	)
-	offset, limit := 0, -1 // 初始值
-	if pageno < 0 {
-		total, err = query.Count()
-		if err != nil || total <= 0 {
-			return -1, err
-		}
-		offset = int(total) + pageno*pagesize
+func Where(query *xorm.Session, conds []interface{}) *xorm.Session {
+	if len(conds) >= 1 {
+		query = query.And(conds[0], conds[1:]...)
 	}
-	// 参数校正
-	if pagesize >= 0 {
-		limit = pagesize
+	return query
+}
+
+func OrWhere(query *xorm.Session, conds []interface{}) *xorm.Session {
+	if len(conds) >= 1 {
+		query = query.Or(conds[0], conds[1:]...)
+	}
+	return query
+}
+
+func Paginate(query *xorm.Session, pageno, pagesize int) *xorm.Session {
+	if pagesize < 0 {
+		return query
+	} else if pagesize == 0 {
+		return query.ID(0)
+	}
+	var offset int
+	if pageno > 0 {
 		offset = (pageno - 1) * pagesize
+	} else if pageno < 0 {
+		total, err := query.Count()
+		if err == nil && total > 0 {
+			offset = pageno * pagesize + int(total)
+		}
 	}
-	if limit >= 0 && offset >= 0 {
-		query = query.Limit(limit, offset)
-	}
-	if total > 0 {
-		err = query.Find(out)
-	} else {
-		total, err = query.FindAndCount(out)
-	}
-	return int(total), err
+	return query.Limit(pagesize, offset)
 }
